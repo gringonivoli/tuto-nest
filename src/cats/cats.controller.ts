19671f1ac888281be8b4b8d05df3be86e2d30146
Maxi Nivoli <m_nivoli@hotmail.com>
2018-05-15 15:26:08 -0300

    init

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+
+  constructor(private readonly catsService: CatsService) { }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  findAll(): Promise<Cat[]> {
+    this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `This action return a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id, @Body() updateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete('id')
+  remove(@Param('id') id) {
+    return `This action removes a #${id} cat`;
+  }
+}
