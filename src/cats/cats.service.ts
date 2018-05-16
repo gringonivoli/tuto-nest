6badb2fcab350f9ee31b1faf866318b4e73c7db5
Maxi Nivoli <m_nivoli@hotmail.com>
2018-05-16 15:58:40 -0300

    working on middlewares

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,14 +3,13 @@ import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
-
   private readonly cats: Cat[] = [];
 
   create(cat: Cat) {
     this.cats.push(cat);
   }
 
-  findAll(): Cat[] {
-    return this.cats;
+  findAll(): Promise<Cat[]> {
+    return Promise.resolve(this.cats);
   }
 }
